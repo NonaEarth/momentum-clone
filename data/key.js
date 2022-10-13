2951634ec99ee0eb817a1f989dc1b387878e3e41
Floppy <hsoj4129@gmail.com>
2022-10-13 23:24:58 +0900

    Update

--- data/key.js
+++ data/key.js
@@ -1,11 +1,11 @@
 let API_KEY_DATA = '';
 
-// Get key data from the local storage.
-if (API_KEY_DATA == '') {
-    API_KEY_DATA = localStorage.getItem('localkeydata');
-}
+// // // Get key data from the local storage.
+// // if (API_KEY_DATA == '') {
+// //     API_KEY_DATA = localStorage.getItem('localkeydata');
+// // }
 
-// You can also save your key with this function.
-// const setKeyData = function (argument) {
-//     localStorage.setItem(localkeydata, argument);
-// }
+// // // You can also save your key with this function.
+// // // const setKeyData = function (argument) {
+// // //     localStorage.setItem(localkeydata, argument);
+// // // }
