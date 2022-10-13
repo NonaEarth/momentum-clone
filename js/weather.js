499980dce624b64cae309bdaeea51a691de81670
Floppy <hsoj4129@gmail.com>
2022-10-13 19:02:38 +0900

    Update

--- js/weather.js
+++ js/weather.js
@@ -7,10 +7,19 @@ function onGeoOk(position) {
     
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     
+    
     fetch(url).then(response => response.json().then(data => {
-
+        
+        
         const weather = document.querySelector('#weather span:first-child');
         const city = document.querySelector('#weather span:last-child');
+        
+        if (data.name === undefined) {
+            weather.innerText = "Couldn't access to the weather API.";
+            weather.style.color = 'tomato';
+            return;
+        }
+        
         city.innerText = data.name;
         weather.innerText = `${data.weather[0].main} / ${data.main.temp} ℃`;
     }));
